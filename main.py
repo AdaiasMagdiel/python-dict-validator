b6f33b9c733ad608887693170f1c53ff0e505178
Don Raulzito <don-raulzito@proton.me>
2023-09-16 19:47:35 -0300

    feat: validator first showcase

---
+++ main.py
@@ -0,0 +1,15 @@
+from pylidator import Validator, Validations
+
+data = {'name': 'John Doe', 'email': 'invalid email.com', 'password': 'error'}
+rules = {
+    'name': [Validations.MIN(6)],
+    'email': [Validations.EMAIL],
+    'password': [Validations.MIN(6)],
+}
+
+validation = Validator(data, rules)
+if validation.has_errors:
+    print('errors:')
+    print(validation.errors)
+else:
+    print(validation.data)
