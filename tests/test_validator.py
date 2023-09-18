3a6e4ca06b8db33f469c8a98c31c3dfa56465566
Adaías Magdiel <adaiasmagdiell@gmail.com>
2023-09-18 19:20:39 -0300

    tests: create alphanumeric and alphanumeric puncts tests

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -31,6 +31,44 @@ def test_alpha(data, rules, expected):
 @pytest.mark.parametrize(
     "data, rules, expected", [
         ({
+            'user': 'admin123'
+        }, {
+            'user': [Validations.ALPHA_NUMERIC]
+        }, False),
+        ({
+            'user': 'admin 123'
+        }, {
+            'user': [Validations.ALPHA_NUMERIC]
+        }, True),
+    ]
+)
+def test_alpha_numeric(data, rules, expected):
+    validation = Validator(data, rules).has_errors
+    assert validation == expected
+
+
+@pytest.mark.parametrize(
+    "data, rules, expected", [
+        ({
+            'role': 'admin.user|admin!guest'
+        }, {
+            'role': [Validations.ALPHA_NUMERIC_PUNCT]
+        }, False),
+        ({
+            'role': 'admin user guest'
+        }, {
+            'role': [Validations.ALPHA_NUMERIC_PUNCT]
+        }, True),
+    ]
+)
+def test_alpha_numeric_punct(data, rules, expected):
+    validation = Validator(data, rules).has_errors
+    assert validation == expected
+
+
+@pytest.mark.parametrize(
+    "data, rules, expected", [
+        ({
             'email': 'valid.email@email.com'
         }, {
             'email': [Validations.EMAIL]
